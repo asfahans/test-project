a4da11150d0f70ca877f89df2ad82a1bf77db23a
Asfahan Shaikh <asfahans@gmail.com>
2020-12-12 19:26:55 +0530

    Heroku upload setting
    Server Configuration for Heroku uploading

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,6 @@ const app = express();
 
 app.use(express.json());
 
-// Routes
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/tickets', ticketRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/vessels', vesselRoutes);
@@ -30,6 +25,19 @@ app.use('/api/upload', uploadRoutes);
 // make upload folder as a static folder for accessibility
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// FOR PRODUCTION USE
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 // Middleware
 
